00099fd783adab640294300911d3e393d0e25a7f
secondlnl <142692021+secondlnl@users.noreply.github.com>
2026-02-09 18:13:42 +0100

    Update questchecker.cs
    + NOW! Loads scenes

--- Assets/Scripts/questchecker.cs
+++ Assets/Scripts/questchecker.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class questchecker : MonoBehaviour
 {
     [SerializeField] private GameObject dialogBox, finishText, notFinishText;
     [SerializeField] private int QuestGoal = 10;
+    [SerializeField] private int levelLoad;
+    [SerializeField] private int levelLoadDelay;
+    private bool loading = false;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -12,14 +17,21 @@ public class questchecker : MonoBehaviour
             if (other.GetComponent<PlayerMovement>().collectedCoins >= QuestGoal)
             {
                 finishText.SetActive(true);
+                loading = true;
+                Invoke("LoadLevel", levelLoadDelay);
             }
             else
                 notFinishText.SetActive(true);
         }
     }
+    private void LoadLevel()
+    {
+        SceneManager.LoadSceneAsync(levelLoad);
+
+    }
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !loading)
         {
             dialogBox.SetActive(false);
             finishText.SetActive(false);
